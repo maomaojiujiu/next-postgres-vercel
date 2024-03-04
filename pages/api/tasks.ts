5cf6cff4b63029d1eb08ac3524e238c81ddb665d
meow <2149268560@qq.com>
2024-03-04 20:49:54 +0800

    在vercel上使用postgres数据库，采用prisma作为orm工具，全栈开发next项目

---
+++ pages/api/tasks.ts
@@ -0,0 +1,35 @@
+// pages/api/tasks.ts
+import {PrismaClient} from '@prisma/client'
+import {NextApiRequest, NextApiResponse} from "next";
+
+const prisma = new PrismaClient()
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        const tasks = await prisma.tasks.findMany()
+        res.status(200).json(tasks)
+    } else if (req.method === 'POST') {
+        const newTask = req.body
+        const createdTask = await prisma.tasks.create({
+            data: newTask
+        })
+        res.status(201).json(createdTask)
+    } else if (req.method === 'PUT') {
+        const {id, content} = req.body
+        const updatedTask = await prisma.tasks.update({
+           where: {id} as any,
+            data: {content} as any
+        })
+        res.status(200).json(updatedTask)
+    } else if (req.method === 'DELETE') {
+        const {id} = req.body
+        const deletedTask = await prisma.tasks.delete({
+            where: {id} as any
+        })
+        res.status(200).json(deletedTask)
+    } else {
+        // Handle any other HTTP method
+        res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE'])
+        res.status(405).end(`Method ${req.method} Not Allowed`)
+    }
+}
